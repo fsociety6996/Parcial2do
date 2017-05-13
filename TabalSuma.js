460f52b5554279fe78516c6b5601c4d470063532
DaevisC <sitaelloup@gmail.com>
2017-05-13 10:02:09 -0400

    parcial

---
+++ TabalSuma.js
@@ -0,0 +1,11 @@
+var x = 7;
+console.log (x+"+1="+(x+1));
+console.log (x+"+2="+(x+2));
+console.log (x+"+3="+(x+3));
+console.log (x+"+4="+(x+4));
+console.log (x+"+5="+(x+5));
+console.log (x+"+6="+(x+6));
+console.log (x+"+7="+(x+7));
+console.log (x+"+8="+(x+8));
+console.log (x+"+9="+(x+9));
+console.log (x+"+10="+(x+10));
